76bab7d96461d541ec977fbabc86c900a6b71493
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-03-22 14:08:03 +0100

    init

---
+++ app/Http/Controllers/PlussController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pluss;
+use Illuminate\Http\Request;
+
+class PlussController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pluss  $pluss
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pluss $pluss)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pluss  $pluss
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pluss $pluss)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pluss  $pluss
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pluss $pluss)
+    {
+        //
+    }
+}
